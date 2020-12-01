4aa90bfcb126e4088c31f3206ee19e5dbde8c82b
Dennis Wenrich <dennisfinley@gmail.com>
2020-11-30 22:52:00 -0500

    started a section tag in the header
    wanted to tie the header and index together

--- the_wooden_crate/header.php
+++ the_wooden_crate/header.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html lang="en">
   <head>
+    <?php wp_head(); ?>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -12,8 +13,9 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-
+    </head>
     <body>
+      <section id="title">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <a href="" class="navbar-brand">The Wooden Crate</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText"
