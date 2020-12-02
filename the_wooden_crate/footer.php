4541e41c839b27c693a6c7e760cd474d001e8311
Dennis Wenrich <dennisfinley@gmail.com>
2020-12-01 19:23:46 -0500

    deleted the bootstrap template / added the js file
    I added the body and html closing tags

--- the_wooden_crate/footer.php
+++ the_wooden_crate/footer.php
@@ -1,21 +1,4 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Cedarville+Cursive&family=Open+Sans:wght@300;400&display=swap"
-        rel="stylesheet">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-
-    <body>
-        <p>This is the footer.</p>
-    </body>
+    <p>This is the footer.</p>
 
     <!-- Optional JavaScript; choose one of the two! -->
 
