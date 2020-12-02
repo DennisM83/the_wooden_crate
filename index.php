3bc41f4dcbbe88e70945a44414066912d5db6291
Dennis Wenrich <dennisfinley@gmail.com>
2020-12-01 19:25:45 -0500

    removed the bootstrap template
    moved the template to the header and footer

---
+++ index.php
@@ -0,0 +1,15 @@
+      <?php get_header(); ?> 
+  <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <h1 id="main-heading" class="display-1"><?php bloginfo('description'); ?></h1>
+                    <button type="button" class="btn btn-dark">Dark</button>
+                    <button type="button" class="btn btn-dark">Dark</button>
+                </div>
+                <div class="col-lg-6">
+                    <img id="raincoat-kid" src="images/raincoat_kid_right.png" alt="photo of a child in a raincoat">
+                </div>
+            </div>
+        </div>
+        </section>
+        <?php get_footer(); ?>
