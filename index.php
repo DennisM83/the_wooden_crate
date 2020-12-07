dd732c95613620046cd767d2afe2c30d159aef46
Dennis Wenrich <dennisfinley@gmail.com>
2020-12-06 21:56:01 -0500

    added 'font awesome' icons to buttons

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
-      <?php get_header(); ?> 
+<?php get_header(); ?> 
   <div class="container">
             <div class="row">
                 <div class="col-lg-6">
-                    <h1 id="main-heading" class="display-1"><?php bloginfo('description'); ?></h1>
-                    <button type="button" class="btn btn-dark">Dark</button>
-                    <button type="button" class="btn btn-dark">Dark</button>
+                    <h1 id="main-heading" class="display-1"><span>Wear.</span><span>Kid's Stuff.</span><span>Meet.</span></h1>
+                    <button type="button" class="btn btn-outline-light btn-lg"><i class="fab fa-apple"></i>Download</button>
+                    <button type="button" class="btn btn-outline-light btn-lg"><i class="fab fa-google-play"></i>Download</button>
                 </div>
                 <div class="col-lg-6">
-                    <img id="raincoat-kid" src="images/raincoat_kid_right.png" alt="photo of a child in a raincoat">
+                    <img id="raincoat-kid" src="<?php echo get_theme_file_uri('/images/raincoat_kid_right.png') ?>" alt="photo of a child in a raincoat">
                 </div>
             </div>
         </div>
         </section>
+        <div class="footer">
         <?php get_footer(); ?>
+        </div>
