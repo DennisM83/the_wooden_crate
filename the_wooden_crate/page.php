593e32f73507bd8b8c9a09fc8c2ee6bace2e8fda
Dennis Wenrich <dennisfinley@gmail.com>
2020-12-10 22:43:06 -0500

    added the smaller headline span

--- the_wooden_crate/page.php
+++ the_wooden_crate/page.php
@@ -1,12 +1,13 @@
     <?php get_header(); ?> 
-    </section>
     <div class="container-fluid">
       <div class="row">
         <div class="col">
+        <span class="smaller-brand">The Wooden Crate</span>
         <?php 
             while(have_posts()) {
             the_post(); ?>
-            
+
+
               <h1><?php the_title(); ?></h1>
 
         <?php }
@@ -15,4 +16,5 @@
         </div>
       </div>
     </div>
+    </section>
     <?php get_footer(); ?>
