807bc71a887321edb1ba8186cb69aa4324c2e358
Dennis Wenrich <dennisfinley@gmail.com>
2020-12-12 21:45:58 -0500

    added a section tag / page id

--- the_wooden_crate/page.php
+++ the_wooden_crate/page.php
@@ -1,22 +1,24 @@
     <?php get_header(); ?>
     <span class="smaller-brand">The Wooden Crate</span>
     </section> 
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col">
-        <?php 
-            while(have_posts()) {
-            the_post(); ?>
+    <section id="page">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col">
+          <?php 
+              while(have_posts()) {
+              the_post(); ?>
 
 
-              <h1><?php the_title(); ?></h1>
+                <h1><?php the_title(); ?></h1>
 
-              <p><?php the_content(); ?></p>
+                <p><?php the_content(); ?></p>
 
-        <?php }
+          <?php }
 
-        ?>
+          ?>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
     <?php get_footer(); ?>
