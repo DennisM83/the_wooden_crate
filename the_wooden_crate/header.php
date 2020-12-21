14e10ebbebd067d50a8fcc0ee757a03a141d13a3
Dennis Wenrich <dennisfinley@gmail.com>
2020-12-20 21:35:02 -0500

    hard coded the navbar links

--- the_wooden_crate/header.php
+++ the_wooden_crate/header.php
@@ -11,14 +11,14 @@
     <body>
       <section id="title">
     <nav class="navbar navbar-expand-lg navbar-light">
-            <a href="" class="navbar-brand">The Wooden Crate</a>
+            <a href="#" class="navbar-brand">The Wooden Crate</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText"
                 aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarText">
                 <ul class="navbar-nav ml-auto">
-                <?php
+                <!--<?php
                     $navitems = array("About", "Pricing", "Download");
                     $count = 0;
 
@@ -27,7 +27,13 @@
                       <a href='#' class='nav-link'>$navitems[$count]</a></li>";
                       $count++;
                     }               
-                  ?>
+                  ?>-->
+                  <li class='nav-item'>
+                    <a href='#' class='nav-link'>About</a>
+                  </li>
+                  <li class='nav-item'>
+                    <a href='#' class='nav-link'>Pricing</a>
+                  </li>
                 </ul>
             </div>
         </nav>
