ba06444d9fe893ad1bd76563887e28b2413467ae
Dennis Wenrich <dennisfinley@gmail.com>
2020-12-22 21:37:36 -0500

    added link back to the home page

--- the_wooden_crate/header.php
+++ the_wooden_crate/header.php
@@ -11,7 +11,7 @@
     <body>
       <section id="title">
     <nav class="navbar navbar-expand-lg navbar-light">
-            <a href="#" class="navbar-brand">The Wooden Crate</a>
+            <a href="<?php echo site_url() ?>" class="navbar-brand">The Wooden Crate</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText"
                 aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
