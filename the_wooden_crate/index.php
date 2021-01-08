7b8bf9343427a46141ecd01599f2f464ed76ec8f
Dennis Wenrich <dennisfinley@gmail.com>
2021-01-07 22:27:35 -0500

    added a dwnload-btn class

--- the_wooden_crate/index.php
+++ the_wooden_crate/index.php
@@ -1,15 +1,18 @@
 <?php get_header(); ?> 
   <div class="container">
             <div class="row">
-                <div class="col-lg-6">
+                <div class="col-6">
+                    <span class="smaller-brand">The Wooden Crate</span>
                     <h1 id="main-heading" class="display-1"><span>Wear.</span><span>Kid's Stuff.</span><span>Meet.</span></h1>
-                    <button type="button" class="btn btn-dark">Dark</button>
-                    <button type="button" class="btn btn-dark">Dark</button>
+                    <button type="button" class="dwnload-btn btn btn-outline-light btn-lg apple"><i class="fab fa-apple"></i> Download</button>
+                    <button type="button" class="dwnload-btn btn btn-outline-light btn-lg play"><i class="fab fa-google-play"></i> Download</button>
                 </div>
-                <div class="col-lg-6">
+                <div class="col-6">
                     <img id="raincoat-kid" src="<?php echo get_theme_file_uri('/images/raincoat_kid_right.png') ?>" alt="photo of a child in a raincoat">
                 </div>
             </div>
         </div>
         </section>
+        <div class="footer">
         <?php get_footer(); ?>
+        </div>
