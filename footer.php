4dc58192abe192b1a8e1ce338db2c8243aa2e96d
Dennis Wenrich <dennisfinley@gmail.com>
2021-01-21 21:44:17 -0500

    added footer links class to links div

---
+++ footer.php
@@ -0,0 +1,33 @@
+    <div class="container-fluid">
+        <div class="row row-cols-1 row-cols-xl-3">
+            <div class="col d-flex justify-content-center align-items-center">
+                <button class="btn footer-btn">The Wooden Crate</button>
+            </div>
+            <div class="footer-links col d-flex justify-content-center align-items-center">
+                <a href="<?php echo site_url('/privacy-policy') ?>">Privacy Policy</a>
+                <a href="">Legal</a>
+                <a href="">Contact Us</a>
+            </div>
+            <div class="social-icons col d-flex justify-content-center align-items-center">
+                <i class="fab fa-3x fa-facebook-square"></i>
+                <i class="fab fa-3x fa-twitter"></i>
+                <i class="fab fa-3x fa-instagram"></i>
+            </div>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+
+    <?php wp_footer(); ?>
+  </body>
+</html>
